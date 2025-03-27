9e734618717e7c43d11b1d6fd26f3c03f49d2295
MatthewD-Dan <bernard.payne@my.uwi.edu>
2025-03-27 23:02:01 +0000

    Added comments

--- App/models/anesthesiologist.py
+++ App/models/anesthesiologist.py
@@ -3,14 +3,29 @@ from .user import User
 from .questionnaire import Questionnaire
 
 class Anesthesiologist(User):
+    """
+    Represents an anesthesiologist user in the database.
+
+    In addition to the standard User fields, an anesthesiologist has a
+    type field set to 'anesthesiologist'.
+    """
     __tablename__ = 'anesthesiologist'
     type = db.Column(db.String(120), nullable=False, default='anesthesiologist')
 
     def __init__(self, firstname, lastname, username, password, email, phone_number):
+        """
+        Constructor for Anesthesiologist.
+
+        """
         super().__init__(firstname, lastname, username, password, email, phone_number)
         self.type = 'anesthesiologist'
 
     def get_json(self):
+        """
+        Return a JSON representation of the Anesthesiologist.
+
+        :return: JSON representation of the Anesthesiologist
+        """
         return{
             'id': self.id,
             'firstname': self.firstname,
@@ -22,6 +37,10 @@ class Anesthesiologist(User):
         }
     
     def update_questionnaire_anesthesiologist(self, questionnaire_id, new_anesthesiologist_notes, status):
+        """
+        Updates the anesthesiologist's notes and status for a given questionnaire.
+        :return: The updated questionnaire
+        """
         questionnaire = Questionnaire.query.get(questionnaire_id)
         if questionnaire:
             try:
