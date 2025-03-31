f534226acc5018c46eb57046687c36fc8510aceb
MatthewD-Dan <bernard.payne@my.uwi.edu>
2025-03-31 17:44:50 +0000

    Removed a test

--- App/tests/patient_unit_test.py
+++ App/tests/patient_unit_test.py
@@ -36,46 +36,5 @@ class PatientUnitTests(unittest.TestCase):
         self.assertEqual(new_patient.username, "johndoe")
         self.assertEqual(new_patient.email, "johndoe@example.com")
         self.assertEqual(new_patient.phone_number, "1234567890")
-
-    def test_create_medical_history(self):
-        new_patient = Patient(
-            firstname="John", 
-            lastname="Doe", 
-            username="johndoe", 
-            password="securepassword", 
-            email="johndoe@example.com", 
-            phone_number="1234567890"
-        )
-
-        update_patient = medical_history(
-            patient_id=new_patient.id,
-            age=30,
-            blood_type="O+",
-            weight=80,
-            height=170,
-            allergies="peanuts",
-            medical_conditions="diabetes",
-            medication="metformin"
-        )
-
-        # Assertions to validate the function
-        self.assertIsNotNone(update_patient)
-        self.assertEqual(update_patient.age, 30)
-        self.assertEqual(update_patient.blood_type, "O+")
-        self.assertEqual(update_patient.weight, 80)
-        self.assertEqual(update_patient.height, 170)
-        self.assertEqual(update_patient.allergies, "peanuts")
-        self.assertEqual(update_patient.medical_conditions, "diabetes")
-        self.assertEqual(update_patient.medication, "metformin")
-
-    def test_get_all_patients(mock_query):
-        # Mock return value
-        mock_query.all.return_value = [
-            Patient("John", "Doe", "johndoe", "pass123", "johndoe@example.com", "1234567890"),
-        ]
-        
-        patients = get_all_patients()
-        assert len(patients) == 1
-        assert patients[0].username == "johndoe"
         
 
