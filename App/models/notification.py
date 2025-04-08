be6b05bd513a75a48f955d7b9c035532dde56296
Honcho TheConquistador <jehlani.joseph@my.uwi.edu>
2025-04-08 00:01:24 +0000

    Removed toDict function Changed patientid to recipient to allow anesthesiologists to receive notifications. Made ids integers

--- App/models/notification.py
+++ App/models/notification.py
@@ -5,32 +5,24 @@ class Notification(db.Model):
     __tablename__ = 'notification'
     
     id = db.Column(db.Integer, primary_key=True)
-    patient_id = db.Column(db.String(220), db.ForeignKey('patient.id'))
+    recipientId = db.Column(db.Integer, db.ForeignKey('user.id'))
     title = db.Column(db.String(220))
     message = db.Column(db.String(220))
     timestamp=db.Column(db.DateTime,default = datetime.now())
     seen = db.Column(db.Boolean, default=False)
     
-    def __init__(self, patient_id, message, title):
-        self.patient_id = patient_id
+    def __init__(self, recipientId, message, title):
+        self.recipientId = recipientId
         self.message = message
         self.title = title
         self.timestamp = datetime.now()
-        
-    
-    def toDict(self):
-        return{
-            'id':self.id,
-            'patient_id': self.patient_id,
-            'message': self.message,
-            'timestamp': self.timestamp.strftime("%Y-%m-%d %H:%M:%S")
-        }
 
     def get_json(self):
         return{
             'id':self.id,
-            'patient_id': self.patient_id,
+            'patient_id': self.recipientId,
             'message': self.message,
             'title': self.title,
-            'timestamp': self.timestamp.strftime("%Y-%m-%d %H:%M:%S")
+            'timestamp': self.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+            'seen': self.seen
         }
