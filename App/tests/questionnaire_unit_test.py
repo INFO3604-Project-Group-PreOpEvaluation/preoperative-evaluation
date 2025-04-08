7b6d92170666fd82b7c1bbe5eb029a97c55c15ef
Bernard Payne <60522543+MatthewD-Dan@users.noreply.github.com>
2025-04-08 02:59:36 -0400

    Added Tests

--- App/tests/questionnaire_unit_test.py
+++ App/tests/questionnaire_unit_test.py
@@ -1,5 +1,4 @@
 import os, tempfile, pytest, logging, unittest
-from werkzerg.security import check_password_hash, generate_password_hash
 
 from App.main import create_app
 from App.database import db, create_db
@@ -14,6 +13,12 @@ from App.controllers import(
     get_questionnaire_by_status_json,
     get_latest_questionnaire
 )
+
+LOGGER = logging.getLogger(__name__)
+
+"""
+    Unit Tests
+"""
 
 class QuestionnaireUnitTests(unittest.TestCase):
 
@@ -38,3 +43,29 @@ class QuestionnaireUnitTests(unittest.TestCase):
         create_questionnaire(patient_id_2, {"q2": "no"})
         questionnaires = get_all_questionnaires()
         self.assertEqual(len(questionnaires), 2)
+
+    def test_get_all_questionnaires_json(self):
+        create_questionnaire("patient123", {"q1": "yes"})
+        json_data = get_all_questionnaires_json()
+        self.assertIsInstance(json_data, list)
+        self.assertGreater(len(json_data), 0)
+
+    def test_get_questionnaire_by_patient_id(self):
+        patient_id = "patient123"
+        create_questionnaire(patient_id, {"q1": "yes"})
+        questionnaire = get_questionnaire_by_patient_id(patient_id)
+        self.assertEqual(questionnaire.patient_id, patient_id)
+
+    def test_get_questionnaire_by_status(self):
+        patient_id = "patient123"
+        responses = {"q1": "yes"}
+        create_questionnaire(patient_id, responses)
+        questionnaires = get_questionnaire_by_status("submitted")
+        self.assertIsInstance(questionnaires, list)
+    
+    def test_get_latest_questionnaire(self):
+        patient_id = "patient123"
+        create_questionnaire(patient_id, {"q1": "yes"})
+        create_questionnaire(patient_id, {"q2": "no"})
+        latest_questionnaire = get_latest_questionnaire(patient_id)
+        self.assertEqual(latest_questionnaire.responses, {"q2": "no"})
